335925fb922d155b57fa3405df1e2461b12992a0
albertocstll70 <89530250+albertocstll70@users.noreply.github.com>
2022-06-26 21:48:24 +0200

    Update NumberPalimdrome.java

--- NumberPalimdrome.java
+++ NumberPalimdrome.java
@@ -13,39 +13,66 @@ public class NumberPalimdrome {
 
 		boolean result = false;
 		boolean leave = false;
-		int palindrome = 0;
-		int multipleElevenThree = 0;
+		int palindrome = 0;	
 		int i = 999;
+		int j=1;
 		ArrayList<Integer> isPalindrome = new ArrayList<Integer>();
+		ArrayList<Integer> multipleEleven = new ArrayList<Integer>();
+		ArrayList<Integer> numPalindrome = new ArrayList<Integer>();
 		
 		
-		//Bucle para buscar el  numero palindromo más grande de 3 cifras y que ademas sea múltiplo de 11
-		while (!leave) {
-			isPalindrome = digit(i);
-			if (isPalindrome(isPalindrome) && i % 11 == 0) {
-				leave = true;
-				multipleElevenThree = i;
-
+		
+		//Bucle para buscar los  numeros multiplo de 11  más grande de 3 cifras y lo guandamos en un ArrayList 
+		while (i>899) {
+			
+			if ( i % 11 == 0) {
+			
+				multipleEleven.add(i);
+				
+				j++;
 			}
 
 			i--;
 		}
-
+		
+       int size = multipleEleven.size();
 		i = 999;
+		j=0;
 		/*Bucle para buscar  que numero de 3 tres cifras  multiplicado 
-		por mayor número palindromo de 3 cifras y múltiplo de 11 produce
+		por mayor número  múltiplo de 11 produce
 		el mayor número palindromo  resultado del producto de dos números de 3 cifras.
 		*/
-		while (!result) {
-
-			palindrome = multipleElevenThree * i;
+		while (!leave) {
+			
+			j=0;
+           while(!result && j<size) {
+			palindrome = multipleEleven.get(j) * i;
 			isPalindrome = digit(palindrome);
-			result = isPalindrome(isPalindrome);
-
-			i--;
+			
+			if(result = isPalindrome(isPalindrome)) {
+				
+				numPalindrome.add(palindrome);
+				result = true;
+				leave = true;
+				
+			}else {
+				j++;
+				
+			}
+			
+			}
+           if(!leave) {
+        	   
+        	   i--;   
+           }
+          
+    
+		
 		}
 
-		System.out.println(multipleElevenThree + " x " + i + "  =  " + palindrome);
+		System.out.println(multipleEleven.get(j) + " x " + i + "  =  " + numPalindrome);
+		
+	
 
 	}
 	//función que   separar los dígitos  de un número  y lo devuelve en un ArrayList
